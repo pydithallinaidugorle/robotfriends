da62fe984f41f490802ca377a316cc1c55ac8dc2
pydithallinaidugorle <gorlepydithallinaidu@gmail.com>
2022-07-17 12:25:57 +0530

    updated Scroll.jsx

--- src/Scroll.jsx
+++ src/Scroll.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Scroll(props) {
   return (
-        <div style = {{overflowY:'scroll', border:'3px solid black', height:'200px', margin: '10px'}}>
+        <div style = {{overflowY:'scroll', border:'5px solid black', height:'200px', margin: '10px'}}>
             {props.children}
         </div>
   )
